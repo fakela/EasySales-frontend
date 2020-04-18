d180d74832fcd049e5742c719ebdb72f5374f764
Fakela <favourkelvin17gmail.com>
2020-04-18 21:41:32 +0100

    initial commit

---
+++ src/components/Product.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="section">
+        <div class="container">
+            <div class="columns" style='min-height:800px;'>
+                <div class="column is-6">
+                    <div class="image is-2by2">
+                        <img src="https://images.unsplash.com/photo-1546856139-46fb2b57ff21?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60">
+                    </div>
+                </div>
+                <div class="column is-5 is-offset-1">
+                    <div class="title is-2">Yummy Cakes</div>
+                    <p class="title is-3 has-text-muted">For as low as s$ 15</p>
+                    <hr>
+                    <br>
+                    <p>inexplicable fairy, bake flat, white sugared, thin, miniature, circular and sacred, alike hot, baked unleavened, assorted creamy, baked meal, regularly hot, increasingly stale, extremely alcoholic, nice spiced, crisp sugary, already delicious, big and magnificent, brown-meal</p>
+                    <hr>
+                    <p class='level'>
+                        <router-link to="/order" class="button is-large is-secondary level-item has-text-centered">Buy Now</router-link>
+                    </p>
+                    <br>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+<style>
+.section{
+    background-color:  rgb(211, 209, 102);}
+</style>
